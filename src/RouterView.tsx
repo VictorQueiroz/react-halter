84973f97295c641576021e9a14f6a9880978ad88
Victor Queiroz <victorcqueirozg@gmail.com>
2019-03-01 00:50:06 -0300

    RouterView: use functionality available on RoutesTree and remove duplicated code

--- src/RouterView.tsx
+++ src/RouterView.tsx
@@ -1,167 +1,67 @@
+import autobind from 'autobind-decorator';
+import { Router } from 'halter';
 import * as React from 'react';
 import { PureComponent } from 'react';
-import { Router } from 'halter';
-// import { History } from 'history';
-import { IRoute } from '../../halter/lib/router';
+import RoutesTree, { IRouteDefinition } from './RoutesTree';
 
-export type ReplaceStateFunction = (state: any, title: string, route: string) => void;
-export type GetComponentFunction = () => (React.ReactNode | Promise<React.ReactNode>);
-
-export interface RouteDefinition {
-    name?: string;
-    path?: string;
-    component?: React.ReactNode;
-    onBefore?: IRoute["onBefore"];
-    getComponent?: GetComponentFunction;
-    childRoutes?: RouteDefinition[];
-}
-
-export interface RouterViewProps {
-    routes: RouteDefinition[];
+export interface IRouterViewProps {
     router: Router;
+    routes: IRouteDefinition[];
 }
 
-export interface RouteRenderInformation {
-    name: string[];
-    path: string[];
-    onBefore: Array<IRoute["onBefore"]>;
-    component: GetComponentFunction[];
-}
-
-export default class RouterView extends PureComponent<RouterViewProps, {
+class RouterView<T extends object = {}> extends PureComponent<T & IRouterViewProps, {
     children?: React.ReactNode;
 }> {
-    public _initPromise?: Promise<void>;
-    constructor(props: RouterViewProps) {
+    public initPromise?: Promise<Router>;
+    private tree = new RoutesTree(this.props.router, this.onReceiveChildren);
+    constructor(props: T & IRouterViewProps) {
         super(props);
         this.state = {};
     }
 
-    fillRouteData(route: RouteDefinition, { name, path, onBefore, component }: RouteRenderInformation) {
-        if(route.name) {
-            name.push(route.name);
-        }
-        if(route.path) {
-            path.push(route.path);
-        }
-        if(route.onBefore) {
-            onBefore.push(route.onBefore);
-        }
-        if(route.component) {
-            const c = route.component;
-            component.push(() => {
-                return c;
-            });
-        } else if(route.getComponent) {
-            component.push(route.getComponent);
-        } else if(!route.childRoutes) {
-            throw new Error('You should either define `getComponent` or `component` property of route');
-        }
-    }
-
-    getRouteData(route: RouteDefinition, parent?: RouteRenderInformation) {
-        const name: string[] = [];
-        const path: string[] = [];
-        const onBefore: Array<IRoute["onBefore"]> = [];
-        const component: GetComponentFunction[] = [];
-        if(parent) {
-            name.push(...parent.name);
-            path.push(...parent.path);
-            if(parent.onBefore) {
-                onBefore.push(...parent.onBefore);
-            }
-            if(parent.component) {
-                component.push(...parent.component);
-            }
-        }
-        this.fillRouteData(route, {
-            name,
-            path,
-            onBefore,
-            component
-        });
-        return {
-            name,
-            path,
-            onBefore,
-            component
-        };
-    }
-
-    async getChildren(component: GetComponentFunction[], props: any): Promise<React.ReactNode | null> {
-        const item = component.shift();
-        if(item) {
-            let Component: any = await item();
-
-            if(Component.__esModule) {
-                Component = Component.default;
-            }
-
-            const children = await this.getChildren(component, props);
-
-            return React.createElement(Component, props, children);
-        }
-        return null;
-    }
-    
-    async createRoutes(routes: RouteDefinition[], parent?: RouteRenderInformation) {
-        for(const route of routes) {
-            if(route.childRoutes) {
-                this.createRoutes(route.childRoutes, this.getRouteData(route, parent));
-                continue;
-            }
-            const data = this.getRouteData(route, parent);
-
-            this.props.router.on({
-                name: data.name.join('.'),
-                path: data.path.join('/').replace(/\/{1,}/g, '/'),
-                async onBefore(match, replaceState, pushState) {
-                    for(const onBefore of data.onBefore) {
-                        if(!onBefore) {
-                            continue;
-                        }
-                        await onBefore(match, replaceState, pushState);
-                    }
-                },
-                callback: async (name, params, query) => {
-                    const children = await this.getChildren(data.component.slice(0), {
-                        location: {
-                            name,
-                            params,
-                            query
-                        }
-                    });
-                    this.setState({
-                        children
-                    });
-                }
-            });
-        }
-    }
-
-    componentWillUnmount() {
-    }
-
-    componentDidMount() {
-        this._initPromise = new Promise(async (resolve, reject) => {
-            try {
-                this.createRoutes(await this.props.routes);
-                await this.props.router.init();
-                resolve();
-            } catch(reason) {
-                reject(reason);
-            }
+    @autobind
+    public onReceiveChildren(children: React.ReactNode) {
+        this.setState({
+            children
         });
     }
 
-    render() {
+    public componentWillUnmount() {
+        this.props.router.destroy();
+    }
+
+    public UNSAFE_componentWillReceiveProps(nextProps: RouterView["props"]) {
+        let router: Router = this.props.router;
+        if(nextProps.router !== router) {
+            router.destroy();
+            router = nextProps.router;
+            this.tree = new RoutesTree(router, this.onReceiveChildren);
+        }
+        if(nextProps.routes !== this.props.routes) {
+            router.destroy();
+            this.tree.createRoutes(nextProps.routes);
+            router.init();
+        }
+    }
+
+    public componentDidMount() {
+        this.initPromise = new Promise((resolve) => {
+            this.tree.createRoutes(this.props.routes);
+            resolve(this.props.router.init());
+        });
+    }
+
+    public render() {
         const {
             children
         } = this.state;
 
-        if(children)
+        if(children) {
             return children;
+        }
 
-        return <div/>;
+        return <span className="router-not-ready"/>;
     }
 }
+
+export default RouterView;
