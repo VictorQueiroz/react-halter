51eee73b9cbcfd9b469efd8637c98ca6fd5b0272
Victor Queiroz <victorcqueirozg@gmail.com>
2019-03-01 00:42:00 -0300

    convert components to class-based components

--- test/react-components.tsx
+++ test/react-components.tsx
@@ -1,66 +1,92 @@
+/* tslint:disable max-classes-per-file */
+
 import * as React from 'react';
 
-export function Home({}: any) {
-    return <div>
-        Home
-    </div>;
+export interface IComponentProps {
+    location: {
+        params: Map<string, string>;
+    };
 }
 
-export function Book({ location: { params: { id } } }: any) {
-    return <div>
-        Book id is "{id}"
-    </div>;
+export class Home extends React.PureComponent<IComponentProps> {
+    public render() {
+        return <div>
+            Home
+        </div>;
+    }
 }
 
-export function Post({ location: { params: { id } } }: any) {
-    return <div>
-        {`Post id is "${id}"`}
-    </div>;
+export class Book extends React.PureComponent<IComponentProps> {
+    public render() {
+        const {location: {params}} = this.props;
+        return <div>
+            Book id is "{params.get('id')}"
+        </div>;
+    }
 }
 
-export function Admin({}: any){
-    return (
-        <div>
-            Welcome to the admin panel!
-        </div>
-    );
+export class Post extends React.PureComponent<IComponentProps> {
+    public render() {
+        const {location: {params}} = this.props;
+        return <div>
+            {`Post id is "${params.get('id')}"`}
+        </div>;
+    }
 }
 
-export function AppWrapper({ children }: any){
-    return (
-        <div>
-            {children}
-        </div>
-    );
-}
-
-export function SpecialWrapper({ children }: any) {
-    return (
-        <div>
-            <h1>This is a special wrapper</h1>
+export class Admin extends React.PureComponent<IComponentProps> {
+    public render() {
+        return (
             <div>
-                {children}
+                Welcome to the admin panel!
             </div>
-        </div>
-    );
+        );
+    }
 }
 
-export function HomeWrapper({ children }: any) {
-    return (
-        <div>
-            This is the home wrapper
-            {children}
-        </div>
-    );
+export class AppWrapper extends React.PureComponent<IComponentProps> {
+    public render() {
+        return (
+            <div>
+                {this.props.children}
+            </div>
+        );
+    }
 }
 
-export function Login({}: any) {
-    return (
-        <div>
-            Login
-            <form action="login.php">
-                <input type="text" placeholder="Password" />
-            </form>
-        </div>
-    );
+export class SpecialWrapper extends React.PureComponent<IComponentProps> {
+    public render() {
+        return (
+            <div>
+                <h1>This is a special wrapper</h1>
+                <div>
+                    {this.props.children}
+                </div>
+            </div>
+        );
+    }
+}
+
+export class HomeWrapper extends React.PureComponent<IComponentProps> {
+    public render() {
+        return (
+            <div>
+                This is the home wrapper
+                {this.props.children}
+            </div>
+        );
+    }
+}
+
+export class Login extends React.PureComponent<IComponentProps> {
+    public render() {
+        return (
+            <div>
+                Login
+                <form action="login.php">
+                    <input type="text" placeholder="Password" />
+                </form>
+            </div>
+        );
+    }
 }
